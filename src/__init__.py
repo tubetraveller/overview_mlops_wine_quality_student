c05f903d1f30bcddd8d27d5fdffec84d76fa88e8
MJ Domenzain <47764531+mjdocevedo@users.noreply.github.com>
2024-06-24 17:14:41 +0200

    Added structure and first scripts

---
+++ src/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("wine_quality/src/config.yaml")
+PARAMS_FILE_PATH = Path("wine_quality/src/models/params.yaml")
+SCHEMA_FILE_PATH = Path("wine_quality/src/data/schema.yaml")
