b152c3ed9407657d7b3ffe5a5ca9fb03d5890f4c
MJ Domenzain <47764531+mjdocevedo@users.noreply.github.com>
2024-06-25 16:33:33 +0200

    Fixed error running dvc repro

--- src/config/config_manager.py
+++ src/config/config_manager.py
@@ -16,8 +16,6 @@ class ConfigurationManager:
             self.config = read_yaml(config_filepath)
             self.params = read_yaml(params_filepath)
             self.schema = read_yaml(schema_filepath)
-
-            create_directories([self.config.root])
             
     def get_data_ingestion_config(self) -> DataIngestionConfig:
           config = self.config.data_ingestion
--- src/config/logger.py
+++ src/config/logger.py
@@ -4,7 +4,7 @@ import logging
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
 
-log_dir = "wine_quality/logs"
+log_dir = "logs"
 log_filepath = os.path.join(log_dir,"running_logs.log")
 os.makedirs(log_dir, exist_ok=True)
 
