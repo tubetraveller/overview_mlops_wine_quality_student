21cba7843e3d3c21ad0f082b9473623311ecc031
MJ Domenzain <47764531+mjdocevedo@users.noreply.github.com>
2024-06-27 11:38:28 +0200

    Added first scripts to be completed

--- src/data/data_validation.py
+++ src/data/data_validation.py
@@ -1,2 +1,24 @@
+import pandas as pd
+from src.config_manager import DataValidationConfig
 
+class DataValidation:
+    def __init__(self, config: DataValidationConfig):
+        self.config = config
+
+    def validate_all_columns(self) -> bool:
+        '''
+        To be completed.
+        Read the dataset, you can help yourself find the right attributes to do so through the src/config.yaml file
+        Check that the columns on the schema.yaml file coincide with those present in the dataset.
+        The variable validation_status will return True if the columns coincide, and false if they don't.
+        '''
+        
+        try:
+            validation_status = None
+            
+
+
+            return validation_status
+        except Exception as e:
+            raise e
         
---
+++ src/models/model_evaluation.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import mlflow
+import mlflow.sklearn
+import dagshub
+import joblib
+from pathlib import Path
+from urllib.parse import urlparse
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
+from src.entity import ModelEvaluationConfig
+from src.common_utils import save_json
+
+# To fill in with your repo information
+dagshub.init(repo_owner='', repo_name='', mlflow=True)
+
+class ModelEvaluation:
+    def __init__(self, config: ModelEvaluationConfig):
+        self.config = config
+
+    def eval_metrics(self, actual, pred):
+        '''
+        To be completed.
+        This function will calculate and return the rmse, mae and r2.
+        '''
+
+        return rmse, mae, r2
+    
+    def log_into_mlflow(self):
+        '''
+        To be completed.
+        This function will load the test set and the trained model and use the dagshub mlflow uri
+        to track the metrics of our model. For this ze will need to make predictions on the test set 
+        and use the previous function (eval_metrics) to evaluate the rmse, mae and r2.
+        Additionally, we'll save these metrics as a dictionary on a json file. You can use the function 
+        found on src/common_utils, save_json.
+        Once again you can use the src/config.yaml file to help you with the attributes.
+        To use mlflow you can use the methods mlflow.log_params, mlflow.log_metric and mlflow.sklearn.log_model.
+        '''
+
+        X_test = pd.read_csv(self.config.test_X_path)
+        y_test = pd.read_csv(self.config.test_y_path)
+        model = joblib.load(self.config.model_path)
+
+        mlflow.set_registry_uri(self.config.mlflow_uri)
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
+
+        with mlflow.start_run():
+            # Making predictions
+
+            # Saving metrics as local
+
+            # Model registry does not work with file store
+            if tracking_url_type_store != "file":
+
+                # Register the model
+                # There are other ways to use the Model Registry, which depends on the use case.
+
+                mlflow.sklearn.log_model(model, "model", registered_model_name="ElasticnetModel")
+
+            else:
+                mlflow.sklearn.log_model(model, "model")           
