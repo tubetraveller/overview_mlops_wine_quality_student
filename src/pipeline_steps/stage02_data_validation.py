89e2e6ebd922e7848c06546ab68308f5e2f79a43
tubetraveller <maxgleichauf@yahoo.de>
2025-03-20 18:08:32 +0000

    Update stage02_data_validation.py

--- src/pipeline_steps/stage02_data_validation.py
+++ src/pipeline_steps/stage02_data_validation.py
@@ -17,6 +17,9 @@ class DataValidationTrainingPipeline:
 
     def main(self):
         config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValidation(config=data_validation_config)
+        data_validation.validate_all_columns()
 
 if __name__ == '__main__':
     try:
