dba2cb572f48149bc28d43c971cca835de406724
tubetraveller <maxgleichauf@yahoo.de>
2025-03-20 18:07:16 +0000

    Update stage01_data_ingestion.py

--- src/pipeline_steps/stage01_data_ingestion.py
+++ src/pipeline_steps/stage01_data_ingestion.py
@@ -21,6 +21,10 @@ class DataIngestionPipeline:
 
     def main(self):
         config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config = data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
 
 if __name__ == '__main__':
     try:
